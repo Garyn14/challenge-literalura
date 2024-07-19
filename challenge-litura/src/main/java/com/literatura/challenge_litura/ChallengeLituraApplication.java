2bd89123250caba62c0833288517848dd255ec3d
Garyn14 <garyn.fernando.hr@gmail.com>
2024-07-19 18:16:46 -0500

    primer commit

---
+++ challenge-litura/src/main/java/com/literatura/challenge_litura/ChallengeLituraApplication.java
@@ -0,0 +1,13 @@
+package com.literatura.challenge_litura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLituraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLituraApplication.class, args);
+	}
+
+}
---
+++ challenge-litura/src/test/java/com/literatura/challenge_litura/ChallengeLituraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.literatura.challenge_litura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLituraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
